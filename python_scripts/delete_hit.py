1fe3295a89b528c476b8524e6a9e59134f45f503
sabari <sabarigirish92@gmail.com>
2017-12-05 17:21:17 -0500

    Adding script to disable HITs.

--- python_scripts/delete_hit.py
+++ python_scripts/delete_hit.py
@@ -1,14 +1,24 @@
+import sys
 
 from create_hit import get_client
 from insert_data_into_mongodb import get_data_path
 
 
 def delete_hit(client, hit_id):
-    client.disable_hit(hit_id=hit_id)
+    response = client.disable_hit(hit_id=hit_id)
 
 
-def main():
-    client = get_client()
+def delete_all_hits(client):
+    response = client.get_all_hits()
+    hit_id_list = list()
+    for hit in response:
+        hit_id_list.append(hit.HITId)
+
+    for hit_id in hit_id_list:
+        delete_hit(client, hit_id)
+
+
+def delete_hits_from_file(client):
     line_number = 0
     hit_id_list = list()
     with open(get_data_path() + '/HITs.txt') as input_file:
@@ -22,4 +32,9 @@ def main():
         delete_hit(client, hit_id)
 
 if __name__ == '__main__':
-    main()
+    client = get_client()
+    argument_length = len(sys.argv)
+    if argument_length == 1:
+        delete_hits_from_file(client)
+    else:
+        delete_all_hits(client)
