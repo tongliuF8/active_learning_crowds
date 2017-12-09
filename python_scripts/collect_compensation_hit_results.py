52656e36cf617dd5526e0f9ae8b866c72e647b5a
Tong Liu <tongliu0314@gmail.com>
2017-12-09 12:27:36 -0500

    change logfile open mode

--- python_scripts/collect_compensation_hit_results.py
+++ python_scripts/collect_compensation_hit_results.py
@@ -18,27 +18,27 @@ def is_worker_info_available(worker_id, collection):
 
 def store_assignement_info_on_submission(client):
 
-    logfile = open(get_log_directory('CompHIT_submission') + get_timestamp() + '.txt', 'w')
+    with open(get_log_directory('CompHIT_submission') + get_timestamp() + '.txt', 'w') as logfile:
 
-    mongo_client = MongoClient('localhost', 8081)
-    db = mongo_client.meteor
-    collection = db[FEEDBACK_COLLECTION]
-    while(True):
-        time.sleep(10)
-        assignment_info = get_workerid_assignmentid(client)
+        mongo_client = MongoClient('localhost', 8081)
+        db = mongo_client.meteor
+        collection = db[FEEDBACK_COLLECTION]
+        while(True):
+            time.sleep(10)
+            assignment_info = get_workerid_assignmentid(client)
 
-        for worker_id in assignment_info:
-            result = is_worker_info_available(worker_id, collection)
-            if not result:
-                assignment_id = assignment_info[worker_id]['assignmentID']
-                feedback = assignment_info[worker_id]['feedback']
-                document = {
-                    'workerID': worker_id,
-                    'assignment_id': assignment_id,
-                    'feedback': feedback
-                }
-                collection.insert_one(document)
-                logfile.write("Worker %s has submitted at %s." % (worker_id, get_timestamp()))
+            for worker_id in assignment_info:
+                result = is_worker_info_available(worker_id, collection)
+                if not result:
+                    assignment_id = assignment_info[worker_id]['assignmentID']
+                    feedback = assignment_info[worker_id]['feedback']
+                    document = {
+                        'workerID': worker_id,
+                        'assignment_id': assignment_id,
+                        'feedback': feedback
+                    }
+                    collection.insert_one(document)
+                    logfile.write("Worker %s has submitted at %s.\n" % (worker_id, get_timestamp()))
 
 def main(environment):
     client = get_client(environment)
