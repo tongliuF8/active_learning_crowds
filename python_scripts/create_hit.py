f8098d244c5471bdcd0ccc7908a5e454bd6db29d
sabari <sabarigirish92@gmail.com>
2017-12-11 19:24:03 -0500

    Modified the create_hit.py to accept the number of HITs and tweets in HITs. Added log file to track the data used in previous HITs to avoid duplication.

--- python_scripts/create_hit.py
+++ python_scripts/create_hit.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     hit_type_id = ""
     if argument_length < 4:
         print("4 arguments required ....\n"
-              "example: python script.py sandbox crowdflower 5 10..")
+              "example: python script.py sandbox crowdflower 5 10")
         sys.exit(0)
 
     environment = sys.argv[1]
