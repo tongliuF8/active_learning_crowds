5a2a9d95dc9d084315b43eac97fcee3ec226eda2
sabari <sabarigirish92@gmail.com>
2017-12-12 18:23:00 -0500

    Adding script to update a HIT expiration time.

---
+++ python_scripts/update_hit_expiration_date.py
@@ -0,0 +1,19 @@
+import sys
+from datetime import datetime
+
+from create_compensation_hit import get_client
+
+
+def update_date(client, datetime_object):
+    client.update_expiration_for_hit(
+        HITId='3V7ICJJAZ9FUCC6QEDH86FOQ0KQ4B2',
+        ExpireAt=datetime_object
+    )
+
+
+if __name__ == '__main__':
+    environment = sys.argv[1]
+    date_time_string = sys.argv[2]
+    client = get_client(environment)
+    datetime_object = datetime.strptime(date_time_string, '%Y-%m-%d-%I:%M%p')
+    update_date(client, datetime_object)
