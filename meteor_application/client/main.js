f2eef2945cf92c0ad3893645340257510688a2a7
sabari <sabarigirish92@gmail.com>
2017-12-13 11:22:24 -0500

    Updating script to add document into the hit collection after completing the HIT.

--- meteor_application/client/main.js
+++ meteor_application/client/main.js
@@ -156,13 +156,6 @@ if(Meteor.isClient){
         'completed': function () {
             if(Session.get('selectedTweet') === NUMBER_OF_TWEETS_IN_HIT) {
                 isComplete = true;
-                hits.insert( {
-                    hitID: hitID,
-                    assignmentID: assignmentID,
-                    workerID: workerID,
-                    tweetList: tweetIDList
-                })
-
             }
             return isComplete;
         }
@@ -230,6 +223,12 @@ if(Meteor.isClient){
             Session.set('selectedTweet',  count);
 
             if(Session.get('selectedTweet') === NUMBER_OF_TWEETS_IN_HIT) {
+                hits.insert( {
+                    hitID: hitID,
+                    assignmentID: assignmentID,
+                    workerID: workerID,
+                    tweetList: tweetIDList
+                });
                 template.find("form").submit();
             }
             template.find("form").reset();
