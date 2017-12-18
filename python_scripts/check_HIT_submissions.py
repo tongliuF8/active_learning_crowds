bed4492c7a777106b4f26f530211ff949b660f1e
Tong Liu <tongliu0314@gmail.com>
2017-12-18 14:33:06 -0500

    improve check function using API

--- python_scripts/check_HIT_submissions.py
+++ python_scripts/check_HIT_submissions.py
@@ -1,7 +1,8 @@
 import sys
-import xml.etree.ElementTree as ET
 from create_compensation_hit import get_client
 from helper_functions import get_timestamp, get_log_directory
+
+MAX_ASSIGNMENTS = 5
 
 def read_HITs_log(file_name):
 
@@ -18,7 +19,7 @@ def read_HITs_log(file_name):
 
     return hit_id_list
 
-def check_submissions(client, hit_id):
+def check_submissions_MTurk(client, hit_id):
 
     hit = client.get_hit(HITId=hit_id)
     print 'HIT {} status: {}'.format(hit_id, hit['HIT']['HITStatus'])
@@ -27,30 +28,28 @@ def check_submissions(client, hit_id):
     NumberOfAssignmentsPending = hit['HIT']['NumberOfAssignmentsPending']
     NumberOfAssignmentsAvailable = hit['HIT']['NumberOfAssignmentsAvailable']
     NumberOfAssignmentsCompleted = hit['HIT']['NumberOfAssignmentsCompleted']
-    print(HITReviewStatus, NumberOfAssignmentsPending, NumberOfAssignmentsAvailable, NumberOfAssignmentsCompleted)
 
     response = client.list_assignments_for_hit(
         HITId=hit_id,
-        AssignmentStatuses=['Submitted'],
     )
     assignments = response['Assignments']
-    print 'The number of submitted assignments is {}'.format(len(assignments))
 
-    for assignment in assignments:
-        WorkerId = assignment['WorkerId']
-        assignmentId = assignment['AssignmentId']
-        assignmentStatus = assignment['AssignmentStatus']
-        answer = assignment['Answer']
-        answer_tree = ET.fromstring(answer)
-        print(WorkerId, assignmentId, assignmentStatus)
+    if len(assignments) != MAX_ASSIGNMENTS:
+        for assignment in assignments:
+            WorkerId = assignment['WorkerId']
+            assignmentId = assignment['AssignmentId']
+            assignmentStatus = assignment['AssignmentStatus']
+            print(hit_id, HITReviewStatus, NumberOfAssignmentsPending, NumberOfAssignmentsAvailable, NumberOfAssignmentsCompleted)
+            print(WorkerId, assignmentId, assignmentStatus)
+    else:
+        print 'The assignments are fully Submitted: {}'.format(len(assignments))
 
 if __name__ == '__main__':
-    environment = sys.argv[1]
-    client = get_client(environment)
+    client = get_client('production')
 
-    file_name = sys.argv[2]
+    file_name = sys.argv[1]
     hit_id_list = read_HITs_log(file_name)
 
     for hit_id in hit_id_list:
-        check_submissions(client, hit_id)
+        check_submissions_MTurk(client, hit_id)
         print
