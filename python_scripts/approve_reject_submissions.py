7772a108d21ca54a4f20bbad64240a8610d1f2d1
Tong Liu <tongliu0314@gmail.com>
2017-12-26 13:14:03 -0500

    approve/reject submission functions

--- python_scripts/approve_reject_submissions.py
+++ python_scripts/approve_reject_submissions.py
@@ -1,6 +1,7 @@
 import sys
 from pymongo import MongoClient
 from helper_functions import get_timestamp, get_log_directory
+from check_HIT_submissions import read_HITs_log
 
 HIT_COLLECTION = 'hit'
 LABEL_COLLECTION = 'label'
@@ -31,64 +32,51 @@ def validate_q3(answer1, answer2):
         return False
 
 
-def get_hitid_list(file_name):
-
-    hit_id_list = list()
-    with open(get_log_directory('HITs') + "/" + file_name) as input_file:
-        line_number = 0
-        for line in input_file:
-            line_number += 1
-            validation = line.strip().split(":")[0]
-            if line_number % 2 == 1 and validation == "Your HIT ID is":
-                hit_id = line.strip().split(":")[1]
-                hit_id_list.append(hit_id.strip())
-
-    return hit_id_list
-
 def check_database_records(hit_id_list, hit_collection, label_collection):
 
-    for index, hit in enumerate(hit_id_list):
-        print(str(index) + "==" + hit + "==")
-        for document in hit_collection.find({'hitID': hit}):
+    for index, hit_id in enumerate(hit_id_list):
+        print(index, hit_id)
+        for document in hit_collection.find({'hitID': hit_id}):
+            print(document.keys())
             assignment_id = document['assignmentID']
             worker_id = document['workerID']
             tweet_id_list = document['tweetList']
 
-            # tweet_id_set = set()
-            # match_count = 0
-            # mismatch = 0
+            # # tweet_id_set = set()
+            # # match_count = 0
+            # # mismatch = 0
 
-            annotations = []
+            # annotations = []
 
-            for tweet_id in tweet_id_list:
-                # if mismatch > 0:
-                #     print("Reject assignment (HITID:{} AssignmentID:{} workerID: {})".format(hit, assignment_id,
-                #                                                                             worker_id))
-                #     break
-                result = label_collection.find({'hitID': hit, 'workerID': worker_id, 'assignmentID': assignment_id, 'id': tweet_id})
-                annotations.append(result)
+            # for tweet_id in tweet_id_list:
+            #     # if mismatch > 0:
+            #     #     print("Reject assignment (HITID:{} AssignmentID:{} workerID: {})".format(hit_id, assignment_id,
+            #     #                                                                             worker_id))
+            #     #     break
+            #     result = label_collection.find({'hitID': hit_id, 'workerID': worker_id, 'assignmentID': assignment_id, 'id': tweet_id})
+            #     annotations.append(result)
 
-            #     if result.count() == 2 and tweet_id not in tweet_id_set:
-            #         if validate_q12(result[0], result[1]):
-            #             if validate_q3(result[0], result[1]):
-            #                 match_count += 1
-            #             else:
-            #                 mismatch += 1
-            #         else:
-            #             mismatch += 1
+            # #     if result.count() == 2 and tweet_id not in tweet_id_set:
+            # #         if validate_q12(result[0], result[1]):
+            # #             if validate_q3(result[0], result[1]):
+            # #                 match_count += 1
+            # #             else:
+            # #                 mismatch += 1
+            # #         else:
+            # #             mismatch += 1
 
-            #     tweet_id_set.add(tweet_id)
-            # if mismatch == 0:
-            #     print("Approve assignment (HITID:{} AssignmentID:{} workerID: {})".format(hit, assignment_id, worker_id))
-            print(len(tweet_id_list), len(annotations), len(tweet_id_list)==len(annotations))
-            print
+            # #     tweet_id_set.add(tweet_id)
+            # # if mismatch == 0:
+            # #     print("Approve assignment (HITID:{} AssignmentID:{} workerID: {})".format(hit_id, assignment_id, worker_id))
+            # print(len(tweet_id_list), len(annotations), len(tweet_id_list)==len(annotations))
+            # print
 
 # def approve_reject_assignments():
 #     # https://boto3.readthedocs.io/en/latest/reference/services/mturk.html#MTurk.Client.approve_assignment
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
-    hit_id_list = get_hitid_list(file_name)
+    hit_id_list = read_HITs_log(file_name)
     print(len(hit_id_list), hit_id_list)
 
     mongo_client = MongoClient('localhost', 8081)
