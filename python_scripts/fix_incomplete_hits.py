51224f89e441658dbf91a6a8ad3b023869935f14
Tong Liu <tongliu0314@gmail.com>
2017-12-28 12:44:17 -0500

    prepare additional assignments for HITs with missing labels

--- python_scripts/fix_incomplete_hits.py
+++ python_scripts/fix_incomplete_hits.py
@@ -99,12 +99,13 @@ def check_submissions_MongoDB(hit_collection, label_collection, MTurk_hits_assig
 
     for k, v in MTurk_hits_assignments.items():
         hit_id = k
-        WorkerId = v[0]
-
         hits_saved = hit_collection.find({'hitID': hit_id}).count()
-        worker_hit_saved = hit_collection.find({'hitID': hit_id, 'workerID': WorkerId}).count()
-        if (hits_saved != MAX_ASSIGNMENTS_PERHIT) or (worker_hit_saved != 1):
-            MongoDB_hit_lost[hit_id] += 1
+
+        for item in v:
+            WorkerId = item[0]
+            worker_hit_saved = hit_collection.find({'hitID': hit_id, 'workerID': WorkerId}).count()
+            if (worker_hit_saved != 1) or (hits_saved != MAX_ASSIGNMENTS_PERHIT):
+                MongoDB_hit_lost[hit_id] += 1
 
     print('MongoDB hit_collection lost: %d' % len(MongoDB_hit_lost))
     # for k, v in OrderedDict(sorted(MongoDB_hit_lost.items(), key=lambda k:k[1])).items():
