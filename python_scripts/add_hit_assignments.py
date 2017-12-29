d1b3f1eb10880bec75229ea5d7fd38dc37c0336d
Tong Liu <tongliu0314@gmail.com>
2017-12-29 14:26:28 -0500

    improve code to create additional assignments

--- python_scripts/add_hit_assignments.py
+++ python_scripts/add_hit_assignments.py
@@ -4,10 +4,11 @@ from create_hit import get_client
 
 def create_additional_assignments(client, hit_id, additional_assignment_perHIT):
     # http://boto.cloudhackers.com/en/latest/ref/mturk.html#boto.mturk.connection.MTurkConnection.extend_hit
-    client.extend_hit(
+    status = client.extend_hit(
         hit_id=hit_id,
         assignments_increment=additional_assignment_perHIT
     )
+    print(status)
 
 if __name__ == '__main__':
     environment = sys.argv[1]
@@ -15,3 +16,4 @@ if __name__ == '__main__':
     additional_assignment_perHIT = 1
     client = get_client(environment)
     create_additional_assignments(client, hit_id, additional_assignment_perHIT)
+    print('HIT %s is extended by %s assignment' % (hit_id, additional_assignment_perHIT))
