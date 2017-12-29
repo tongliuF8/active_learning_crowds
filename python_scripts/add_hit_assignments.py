e553a963c98f1379698e7fb720585208725c9ad8
Tong Liu <tongliu0314@gmail.com>
2017-12-29 11:07:16 -0500

    switch to boto2 client

--- python_scripts/add_hit_assignments.py
+++ python_scripts/add_hit_assignments.py
@@ -1,11 +1,12 @@
 import sys
 
-from create_compensation_hit import get_client
+from create_hit import get_client
 
 def create_additional_assignments(client, hit_id, additional_assignment_perHIT):
-    client.create_additional_assignments_for_hit(
-        HITId=hit_id,
-        NumberOfAdditionalAssignments=additional_assignment_perHIT
+    # http://boto.cloudhackers.com/en/latest/ref/mturk.html#boto.mturk.connection.MTurkConnection.extend_hit
+    client.extend_hit(
+        hit_id=hit_id,
+        assignments_increment=additional_assignment_perHIT
     )
 
 if __name__ == '__main__':
