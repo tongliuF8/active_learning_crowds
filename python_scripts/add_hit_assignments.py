9b5e0726bab6bc91b24ca8ce0b6ef7dabaee7199
Tong Liu <tongliu0314@gmail.com>
2017-12-29 17:49:17 -0500

    polish code

--- python_scripts/add_hit_assignments.py
+++ python_scripts/add_hit_assignments.py
@@ -5,26 +5,6 @@ from datetime import datetime
 # boto3
 from create_compensation_hit import get_client
 
-def check_hit_status(client, hit_id):
-
-    hit = client.get_hit(HITId=hit_id)
-    # hit.keys() = [u'HIT', 'ResponseMetadata']
-
-    # hit['HIT'].keys() = [u'HITGroupId', u'RequesterAnnotation', u'NumberOfAssignmentsCompleted', u'Description', u'MaxAssignments', u'Title', u'NumberOfAssignmentsAvailable', u'Question', u'CreationTime', u'AssignmentDurationInSeconds', u'HITTypeId', u'NumberOfAssignmentsPending', u'HITStatus', u'HITId', u'QualificationRequirements', u'Keywords', u'Expiration', u'Reward', u'HITReviewStatus', u'AutoApprovalDelayInSeconds']
-
-    # hit['ResponseMetadata'].keys() = ['RetryAttempts', 'HTTPStatusCode', 'RequestId', 'HTTPHeaders']
-
-    HITStatus = hit['HIT']['HITStatus']
-    HITCreationTime = hit['HIT']['CreationTime'].strftime("%Y-%m-%d %H:%M:%S")
-    HITExpiration = hit['HIT']['Expiration'].strftime("%Y-%m-%d %H:%M:%S")
-    HITReviewStatus = hit['HIT']['HITReviewStatus']
-    NumberOfAssignmentsPending = hit['HIT']['NumberOfAssignmentsPending']
-    NumberOfAssignmentsAvailable = hit['HIT']['NumberOfAssignmentsAvailable']
-    NumberOfAssignmentsCompleted = hit['HIT']['NumberOfAssignmentsCompleted']
-
-    print(HITStatus, HITCreationTime, HITExpiration, HITReviewStatus, NumberOfAssignmentsPending, NumberOfAssignmentsAvailable, NumberOfAssignmentsCompleted)
-    print
-
 def update_date(client, hit_id):
 
     date_time_string = "2018-01-05 12:00:00"
@@ -61,10 +41,8 @@ if __name__ == '__main__':
     client = get_client(environment)
 
     print(hit_id)
-    check_hit_status(client, hit_id)
+
     # update_date(client, hit_id)
 
     # create_additional_assignments_boto2(client, hit_id, additional_assignment_perHIT)
-    create_additional_assignments(client, hit_id, additional_assignment_perHIT)
-
-    check_hit_status(client, hit_id)    
+    # create_additional_assignments(client, hit_id, additional_assignment_perHIT)
